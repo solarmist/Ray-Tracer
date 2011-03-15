a8e3acd441b4d6e5554a487c1fb672864c644660
Joshua Olson <solarmist@Yachiru.local>
2011-03-15 15:19:38 -0700

    Initial Commit

---
+++ RayTracer/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RayTracer
+//
+//  Created by Joshua Olson on 3/15/11.
+//  Copyright 2011 solarmist. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
