decea7c8ed082cb0b7884a06d4d5d241803864ae
Joshua Olson <joshua.olson@gmail.com>
2011-03-18 16:16:46 -0700

    Added multisampling of pixels

--- RayTracer/main.cpp
+++ RayTracer/main.cpp
@@ -9,63 +9,73 @@
 
 #include <vector>
 #include <iostream>
-#include "Vector3.h"
-#include "rgb.h"
 #include "Image.h"
+#include "rgb.h"
+#include "Sample.h"
 #include "Shape.h"
-#include "Triangle.h"
 #include "Sphere.h"
+#include "Triangle.h"
+#include "Vector2.h"
+#include "Vector3.h"
 using namespace std;
 
 int main ()
 {
-    cout<<"Test";
-    
     HitRecord rec;
     bool is_a_hit;
     precision tmax;         //Max valid t parameter
     Vector3 dir(0, 0, -1);  //Direction of viewing rays
+    const int numSamples = 1;
+    Vector2 samples[numSamples];
     int height, width;
-    height = 500;
-    width = 500;
+    height = 900;
+    width = 1200;
     rgb background(.2f, .2f, .2f);
     
     //Geometery
     vector<Shape *> shapes;
-    shapes.push_back(new Sphere(Vector3(250, 250, -1000), 
-                                150, 
+    shapes.push_back(new Sphere(Vector3(500, 500, -2000), 
+                                300, 
                                 rgb(.2f, .2f, .8f)) );
-    shapes.push_back(new Triangle(Vector3(300.0f, 600.0f, -800.0f), 
-                                  Vector3(0.0f, 100.0f, -10000.0f), 
-                                  Vector3(450.0f, 20.0f, -100.0f), 
+    shapes.push_back(new Triangle(Vector3(600.0f, 1000.0f, -1600.0f), 
+                                  Vector3(0.0f, 200.0f, -10000.0f), 
+                                  Vector3(900.0f, 40.0f, -200.0f), 
                                   rgb(.8f, .2f, .2f)));
     
     Image im(width, height);
     
+    multiJitter(samples, numSamples);
+    
     //Loop over pixels
     for (int i = 0; i < width; i++) 
         for (int j = 0; j < height; j++) {
+            rgb color(0,0,0);
             tmax = 100000.0f;
             is_a_hit = false;
-            Ray r(Vector3(i, j, 0), dir);
-            
-            //Loop over list of Shapes
-            for (int k = 0; k < shapes.size(); k++)
-                if(shapes[k]->hit(r, .00001f, tmax, 0.0f, rec)){
-                    tmax = rec.t;
-                    is_a_hit = true;
-                }
-            
-            if(is_a_hit)
-                im.set(i, j, rec.color);
-            else
-                im.set(i, j, background);
+
+            //Do all the samples for each pixel and average the color
+            for (int multi = 0; multi < numSamples; multi++) {
+                Ray r(Vector3(i - 0.5f +samples[multi].x(), j - 0.5f + samples[multi].y(), 0), dir);
+                //Loop over list of Shapes
+                for (int k = 0; k < shapes.size(); k++)
+                    if(shapes[k]->hit(r, .00001f, tmax, 0.0f, rec)){
+                        tmax = rec.t;
+                        is_a_hit = true;
+                    }
+                
+                if(is_a_hit)
+                    color += rec.color / numSamples;
+                else
+                    color += background / numSamples;
+
+            }
+            im.set(i, j, color);
         }
     fstream file;
     file.open("/Users/solarmist/Documents/Graphics/RayTracer/test.ppm", fstream::out);
-    im.writePPM(cout);
     im.writePPM(file);
     file.close();
+    cout << "Done" << endl;
     
     return 0;
 }
