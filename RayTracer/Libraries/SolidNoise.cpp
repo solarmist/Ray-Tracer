c61830aad6736416329ecf29b07278a9d49d989e
Joshua Olson <joshua.olson@gmail.com>
2011-03-20 19:30:51 -0700

    Made classes more consistent.

---
+++ RayTracer/Libraries/SolidNoise.cpp
@@ -0,0 +1,10 @@
+//
+//  SolidNoise.cpp
+//  RayTracer
+//
+//  Created by Joshua Olson on 3/20/11.
+//  Copyright 2011 solarmist. All rights reserved.
+//
+
+#include "SolidNoise.h"
+
---
+++ RayTracer/Libraries/SolidNoise.h
@@ -0,0 +1,12 @@
+//
+//  SolidNoise.h
+//  RayTracer
+//
+//  Created by Joshua Olson on 3/20/11.
+//  Copyright 2011 solarmist. All rights reserved.
+//
+#ifndef _SOLID_NOISE_H_
+#define _SOLID_NOISE_H_ 1
+
+
+#endif //_SOLID_NOISE_H_
